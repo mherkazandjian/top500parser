e6785baab11eac9a0d685a6032acbbb127a874c9
Mher Kazandjian <mherkazandjian@gmail.com>
2018-10-13 05:03:53 +0200

    comment all the functions and the header of the main script

--- all_in_one.py
+++ all_in_one.py
@@ -1,17 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
 """
 <keywords>
-test, python
+top, 500, hpc, list, web, scrapping
 </keywords>
 <description>
-'https://www.top500.org/lists/top500/'
+Script that scrapps the top500 list HPC website
 
-pip install requests BeautifulSoup4
+The system specs of each system is fetched for all the releases dating
+back to 1993 (as of the time of writing this script)
+
+.. see-also: https://www.top500.org/lists/top500
 </description>
 <seealso>
 </seealso>
 """
 import time
-import os
 import pickle
 import logging
 import datetime as dt
@@ -26,8 +30,19 @@ logger.setLevel(logging.INFO)
 
 
 class MyFormatter(logging.Formatter):
+    """
+    Custom formatter for stdout
+    """
     converter = dt.datetime.fromtimestamp
+
     def formatTime(self, record, datefmt=None):
+        """
+        Custom time formatter
+
+        :param record: a record
+        :param datefmt: the date time format
+        :return:
+        """
         ct = self.converter(record.created)
         if datefmt:
             t_str = ct.strftime(datefmt)
@@ -45,11 +60,12 @@ console.setFormatter(formatter)
 logger.addHandler(console)
 
 
-def find_unique_top_500_lists(base_url):
+def find_unique_top_500_lists(base_url: str) -> list:
     """
+    Find the list of all the released lists in the format YYYY/MM
 
-    :param base_url:
-    :return:
+    :param base_url: the url of the top500 list
+    :return: list of strings
     """
     logger.info('fetch all releases urls')
     response = requests.get(base_url + '/lists/top500')
@@ -76,11 +92,17 @@ def find_unique_top_500_lists(base_url):
     return sorted(lists_dates)
 
 
-def find_entries_in_top_500_list(base_url, release_date):
+def find_entries_in_top_500_list(base_url: str, release_date: str) -> dict:
     """
+    Fetch the systems in a particular release
 
-    :param release_date:
-    :return:
+    For example, a certain release date 2001/06 is used along with the base url
+    https://www.top500.org to fetch the list of system in that release. Since
+    it is a top500 list, the returned dict contains the info of 500 systems
+
+    :param base_url: the url of the top 500 list HPC systems website
+    :param release_date: the release date e.g 2001/06
+    :return: the info of all the systems of the specified release
     """
     logger.info(f'\tfetch info of release {release_date}')
     systems = {}
@@ -92,10 +114,13 @@ def find_entries_in_top_500_list(base_url, release_date):
     return systems
 
 
-def find_entries_in_top_500_list_page(base_url, page_url):
+def find_entries_in_top_500_list_page(base_url: str, page_url: str) -> dict:
     """
+    Return the info of the systems of a particlular page
 
-    :return:
+    :param base_url: the base url of the top 500 list website
+    :param page_url: the url of the a page of a certain release
+    :return: dict of systems info that are on the page_url
     """
     logger.info(f'\t\tfind systems on page {page_url}')
     response = requests.get(page_url)
@@ -112,17 +137,19 @@ def find_entries_in_top_500_list_page(base_url, page_url):
         system_url = f"{base_url}{system_tag['href']}"
         system_rank, system_info = fetch_system_details(system_url)
         systems_info[system_rank] = system_info
+        # DEBUG
         # if system_index == 1:
         #     break
-        time.sleep(1.0)
+        # time.sleep(1.0)
 
     return systems_info
 
 
-def fetch_system_details(system_url):
+def fetch_system_details(system_url: str) -> tuple:
     """
+    Fetch the system info from the system url
 
-    :return:
+    :return: dict
     """
     logger.info(f'\t\t\tfind system info {system_url}')
     response = requests.get(system_url)
@@ -148,29 +175,30 @@ def fetch_system_details(system_url):
     return system_rank, system_info
 
 
-available_lists = find_unique_top_500_lists(top500_lists)
+def main():
+    available_lists = find_unique_top_500_lists(top500_lists)
 
-with open(os.path.expanduser('~/tmp/top500.pkl'), 'wb') as fobj:
+    with open('top500_all_releases.pkl', 'wb') as fobj:
 
-    all_releases_info = {}
+        all_releases_info = {}
 
-    for list_release_date in list(reversed(available_lists)):
+        for list_release_date in list(reversed(available_lists)):
 
-        list_release_info = find_entries_in_top_500_list(
-            top500_lists,
-            list_release_date
-        )
+            list_release_info = find_entries_in_top_500_list(
+                top500_lists,
+                list_release_date
+            )
 
-        fname = os.path.expanduser(
-            f"~/tmp/sandbox/top500_{list_release_date.replace('/', '_')}.pkl"
-        )
-        with open(fname, 'wb') as _fobj:
-            pickle.dump(list_release_info, _fobj)
-        logger.info(f'\tdump release info {fname}')
+            fname = f"top500_{list_release_date.replace('/', '_')}.pkl"
+            with open(fname, 'wb') as _fobj:
+                pickle.dump(list_release_info, _fobj)
+            logger.info(f'\tdump release info {_fobj.name}')
 
-        all_releases_info[list_release_date] = list_release_info
+            all_releases_info[list_release_date] = list_release_info
 
-    pickle.dump(all_releases_info, fobj)
-    logger.info(f'dump release info {fname}')
+        pickle.dump(all_releases_info, fobj)
+        logger.info(f'dump release info {fobj.name}')
 
-print('done')
+
+if __name__ == '__main__':
+    main()
